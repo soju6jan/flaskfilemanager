2d3cfc49603652378634fe9719ebbbc4edac1afe
soju6jan <cybersol@naver.com>
2020-02-10 10:18:39 +0900

    1

--- __init__.py
+++ __init__.py
@@ -16,3 +16,13 @@ def plugin_unload():
     pass
 
 
+plugin_info = {
+    'version' : '0.1.0',
+    'name' : 'FileManager'',
+    'category_name' : 'service',
+    'icon' : '',
+    'developer' : 'soju6jan',
+    'description' : 'FileManager',
+    'home' : 'https://github.com/soju6jan/flaskfilemanager',
+    'more' : '',
+}
