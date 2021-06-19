1690459cdaeb673038851d48a09e4f9f0c9d9c84
soju6jan <cybersol@naver.com>
2021-06-19 21:16:04 +0900

    update

--- __init__.py
+++ __init__.py
@@ -2,15 +2,16 @@
 """
 Rich File Manager for Flask
 """
-
 from .filemanager import filemanager_blueprint as blueprint
 from .filemanager import init, set_access_control_function
 
+
 __author__ = 'Stephen Brown (Little Fish Solutions LTD)'
 
+
 def plugin_load():
-    from framework import app
-    app.config['FLASKFILEMANAGER_FILE_PATH'] = r'/'
+    from framework import app, path_app_root
+    app.config['FLASKFILEMANAGER_FILE_PATH'] = path_app_root
     init(app)
 
 def plugin_unload():
@@ -18,13 +19,15 @@ def plugin_unload():
 
 
 plugin_info = {
-    'version' : '0.1.0',
-    'name' : 'FileManager',
-    'category_name' : 'tool',
+    'version' : '1.0.0',
+    'name' : '파일 매니저',
+    'category_name' : 'system',
     'icon' : '',
     'developer' : 'soju6jan',
-    'description' : 'FileManager',
+    'description' : 'RichFilemanager를 Flask에서 동작하도록 한 FlaskFileManager 포크',
     'home' : 'https://github.com/soju6jan/flaskfilemanager',
     'more' : '',
 }
 
+from tool_base import ToolUtil
+ToolUtil.save_dict(plugin_info, os.path.join(os.path.dirname(__file__), 'info.json'))
